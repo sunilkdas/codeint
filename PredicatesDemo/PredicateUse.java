5dacf59997546cec14f467ba3992514cfc608f68
Sunil Kumar Das <b311055@iiit-bh.ac.in>
2019-12-25 23:20:21 +0530

    Update PredicateUse.java

--- PredicatesDemo/PredicateUse.java
+++ PredicatesDemo/PredicateUse.java
@@ -1,1 +1,15 @@
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateUse {
+	
+public static void main(String[] args) {
+	List<Integer> x=Arrays.asList(1,2,3,4,3,4,2,2,2,4,2,2,2,3);
+	Predicate<Integer> p=Helper.noGt2;
+	x.forEach(item -> {
+		System.out.println(p.test(item)==true?item:"");
+	});
+}
+}
