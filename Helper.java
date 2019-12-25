635a4cc83075c29fb85a3302d6922c0cec77ec4e
Sunil Kumar Das <b311055@iiit-bh.ac.in>
2019-12-25 23:21:13 +0530

    Delete Helper.java

--- Helper.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Predicate;
-
-public class Helper {
-public static  Predicate<Integer> noGt2= new Predicate<Integer>() {
-	public  boolean test(Integer i) {
-		return i>2? true:false;
-	}
-};
-}
